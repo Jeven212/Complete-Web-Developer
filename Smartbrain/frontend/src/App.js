2c5db0c0d4cccd87c6f404aed49c9801c5a7cf4f
Jeven212 <jeven.seider@web.de>
2022-10-30 22:52:18 +0100

    Stand der Dinge

--- Smartbrain/frontend/src/App.js
+++ Smartbrain/frontend/src/App.js
@@ -1,14 +1,20 @@
+window.process = {
+  ...window.process
+};
+console.log("process111", window.process)
+
 import './App.css';
 import { Component } from 'react';
 import ParticlesBG from "particles-bg";
-// import Clarifai from "clarifai";
+import Clarifai from "clarifai";
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';  
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-const Clarifai = require('clarifai');
 
+console.log("process222", window.process)
+ 
 const app = new Clarifai.App({
   apiKey: "56610c0daef641aead61e7de0d0781bd"
 });
@@ -22,7 +28,6 @@ class App extends Component {
     };
   }
 
-
   onInputChange = (event) => {
     console.log(event.target.value);
     this.setState({input: event.target.value});
@@ -32,17 +37,17 @@ class App extends Component {
     console.log("BTN", this.state.input);
     this.setState({imgURL: this.state.input});
 
-    // app.models.predict(
-    //   "a403429f2ddf4b49b307e318f00e528b", 
-    //   "https://samples.clarifai.com/face-det.jpg")
-    //   .then(
-    //     function (response) {
-    //       console.log(response)
-    //     },
-    //     function (error){
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b", 
+      "https://samples.clarifai.com/face-det.jpg")
+      .then(
+        function (response) {
+          console.log(response)
+        },
+        function (error){
 
-    //     }
-    // );
+        }
+    );
   }
 
   render() {
