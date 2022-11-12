14ef7f684d70646eabd32f62bf9dcd6ca6477e05
Jeven212 <jeven.seider@web.de>
2022-11-12 17:32:41 +0100

    node excersise

--- Smartbrain/backend/server.js
+++ Smartbrain/backend/server.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/hello", (request, response) => {
-    response.send("Hiiii there!!!")
+app.use(express.json());
+
+// app.use(express.static(__dirname + "/public"));
+
+app.get("/hello", (req, res) => {
+    res.send("Hiiii there!!!");
+});
+
+app.get("/", (req, res) => {
+    console.log("okay");
+    res.send(200);
+});
+
+app.get("/json", (req, res) => {
+    const { name, region, time } = req.body;
+    console.log("Got JSON!");
+    console.log(name, "lives in", region,time, "!");
+    res.send(200);
 });
 
 app.listen(3000);
